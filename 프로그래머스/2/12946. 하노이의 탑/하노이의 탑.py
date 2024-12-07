45d23857432d1e2dcc4baf6262580f1d1cd6bfe9
박규원 <gw2000sk@korea.ac.kr>
2024-12-08 01:56:27 +0900

    [level 2] Title: 하노이의 탑, Time: 10.47 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -6,13 +6,15 @@ def solution(n):
 
 def hanoi(n, start , end , answer):
     if(n==1):
-        answer.append([start, end])
+        answer.append([start,end])
         return
         
     empty = 6-start-end
     hanoi(n-1, start, empty, answer)
     hanoi(1, start, end, answer)
     hanoi(n-1, empty, end, answer)
+    
+    return
     
     
     
