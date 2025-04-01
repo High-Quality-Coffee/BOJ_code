b0f68d8023f45dfa35fdd763a6b56d0e66995e39
박규원 <gw2000sk@korea.ac.kr>
2025-04-01 12:45:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35252 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,57 +1,49 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-#dfs
-def dfs(graph,v,Dvisited):
-  Dvisited[v]=True
-  print(v,end=' ')
-  
-  for i in graph[v]:
-    if(Dvisited[i]==False):
-      dfs(graph,i,Dvisited)
+n,m,v = map(int,input().split())
 
-#bfs 
-def bfs(graph,start,Bvisited):
-  queue=deque([start])
-  Bvisited[start]=True
-  
-  while queue:
-    v=queue.popleft()
-    print(v,end=' ')
+ary = [ [] for _ in range(n+1) ]
+q = deque()
 
-    for i in graph[v]:
-      if(Bvisited[i]==False):
-        queue.append(i)
-        Bvisited[i]=True
-  
+d_visited = [False] * (n+1)
+b_visited = [False] * (n+1)
 
-#입력받은 값을 공백을 기준으로 분리
-n,m,v=input().split()
-n,m,v=int(n),int(m),int(v)
 
-graph=[[] for i in range(n+1)]
-
-arr=[[0]*2 for i in range(n)]
-  
 for i in range(m):
-   #arr[i][j],arr[i][j+1]=input().split()
-   #arr[i][j],arr[i][j+1]=int(arr[i][j]),int(arr[i][j+1])
-    a,b=input().split()
-    a,b=int(a),int(b)
-  
-    if(graph[a].count(b)==0):
-      graph[a].append(b)
-    if(graph[b].count(a)==0):
-      graph[b].append(a)
-  
+    s,e = map(int,input().split())
+    ary[s].append(e)
+    ary[e].append(s)
 
-for i in range(1,n+1):
-  graph[i].sort()
+for i in range(n+1):
+    ary[i].sort()
 
-Dvisited=[False]*(n+1)
-Bvisited=[False]*(n+1)
+def bfs(v):
+    q.append(v)
+    b_visited[v] = True
 
-dfs(graph,v,Dvisited)
+    while q:
+        node = q.popleft()
+        print(node, end=' ')
+        for i in ary[node]:
+            if not b_visited[i]:
+                q.append(i)
+                b_visited[i] = True
+
+
+
+def dfs(v):
+    d_visited[v] = True
+
+    print(v , end=' ')
+
+    for i in ary[v]:
+        if not d_visited[i]:
+            dfs(i)
+
+
+dfs(v)
 print()
-bfs(graph,v,Bvisited)
-
+bfs(v)
 
