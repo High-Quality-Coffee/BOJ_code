def07a162e99d4610d6597b4d01e78c69054233d
Park GyuWon <gw2000sk@korea.ac.kr>
2025-07-28 03:18:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 86080 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count=0;
+        int value=666;
+
+        while(true){
+
+            if(String.valueOf(value).contains("666")){
+                count++;
+                if(count==n) break;
+            }
+
+            value++;
+        }
+
+        System.out.println(value);
+    }
+}
