c5fd5aebab357b647cbab6558de17cbd6e53d366
Park GyuWon <gw2000sk@korea.ac.kr>
2025-08-01 00:08:38 +0900

    [Bronze V] Title: 오늘 날짜, Time: 92 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // 현재 시간        
+        System.out.println("2025-08-01");
+    }
+}
